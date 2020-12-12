bbc73a1210954def0ce5c29d72b8c65f32afc1a7
rsnyder <ronald.snyder@ithaka.org>
2020-12-12 03:17:08 +0000

    gitpod config

--- app/bin/run-dev.sh
+++ app/bin/run-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /workspace/visual-essays/app/client-lib; yarn; nohup yarn serve & 
-#/workspace/visual-essays/app/server/main.py -l info -d
+/workspace/visual-essays/app/server/main.py -l info -d
