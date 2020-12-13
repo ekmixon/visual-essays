71845b35b8d56f3202fb36056293a8f7009def08
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-12-13 09:00:33 -0500

    deploy testing

--- app/bin/gcr-deploy.sh
+++ app/bin/gcr-deploy.sh
@@ -29,8 +29,10 @@ if output="$(git status --porcelain)" && [ -z "$output" ]; then
   cp -va app/Dockerfile gcr-build
   cp -va app/server/*.py app/server/gh-token app/server/visual-essay-gcreds.json app/server/*.txt app/server/*.html app/server/sparql app/server/mappings gcr-build/server
   cp index.html gcr-build
-  cat index.html | sed "s/APP_VERSION/$APP_VERSION/" | sed 's/\/visual-essays\/js\//\/static\/js\//' > gcr-build/index.html
+  cat index.html | sed "s/APP_VERSION/$APP_VERSION/" | sed 's/\/visual-essays\/js\//\/static\/js\//' | sed 's/\/visual-essays\/app\/client-lib\/public\/css\//static\/css\//' > gcr-build/index.html
   cp -va app/client-lib/components gcr-build
+  cp -va app/client-lib/public/css gcr-build
+
   cp -va js gcr-build
   cp -va images gcr-build
 
