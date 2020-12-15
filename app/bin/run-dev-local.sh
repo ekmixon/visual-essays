4156c5f01a16f332dd5376ad8382c3722acdd13d
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-12-15 11:57:10 -0500

    Enable setting content root from convenience script

--- app/bin/run-dev-local.sh
+++ app/bin/run-dev-local.sh
@@ -3,11 +3,15 @@
 CWD=`pwd`
 echo $CWD
 
+CONTENT_ROOT=${1:-$CWD}
+
+echo CONTENT_ROOT $CONTENT_ROOT
+
 cd $CWD/app/client-lib
 yarn serve &
 P1=$!
 
-$CWD/app/server/main.py -l info -c $CWD
+$CWD/app/server/main.py -l info -c $CONTENT_ROOT
 P2=$!
 
 wait $P1 $P2
